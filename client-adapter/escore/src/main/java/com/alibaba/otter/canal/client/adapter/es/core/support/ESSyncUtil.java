3a23b458e0e94da7cf6ee5796f4e30f749847914
xiangyang <il>
2021-01-07 11:55:42 +0800

    修改日期格式

--- client-adapter/escore/src/main/java/com/alibaba/otter/canal/client/adapter/es/core/support/ESSyncUtil.java
+++ client-adapter/escore/src/main/java/com/alibaba/otter/canal/client/adapter/es/core/support/ESSyncUtil.java
@@ -113,9 +113,9 @@ public class ESSyncUtil {
             } else if (val instanceof java.sql.Timestamp) {
                 DateTime dateTime = new DateTime(((java.sql.Timestamp) val).getTime());
                 if (dateTime.getMillisOfSecond() != 0) {
-                    res = dateTime.toString("yyyy-MM-dd'T'HH:mm:ss.SSS" + Util.timeZone);
+                    res = dateTime.toString("yyyy-MM-dd HH:mm:ss.SSS");
                 } else {
-                    res = dateTime.toString("yyyy-MM-dd'T'HH:mm:ss" + Util.timeZone);
+                    res = dateTime.toString("yyyy-MM-dd HH:mm:ss");
                 }
             } else if (val instanceof java.sql.Date || val instanceof Date) {
                 DateTime dateTime;
@@ -129,9 +129,9 @@ public class ESSyncUtil {
                     res = dateTime.toString("yyyy-MM-dd");
                 } else {
                     if (dateTime.getMillisOfSecond() != 0) {
-                        res = dateTime.toString("yyyy-MM-dd'T'HH:mm:ss.SSS" + Util.timeZone);
+                        res = dateTime.toString("yyyy-MM-dd HH:mm:ss.SSS" );
                     } else {
-                        res = dateTime.toString("yyyy-MM-dd'T'HH:mm:ss" + Util.timeZone);
+                        res = dateTime.toString("yyyy-MM-dd HH:mm:ss");
                     }
                 }
             } else if (val instanceof Long) {
@@ -140,9 +140,9 @@ public class ESSyncUtil {
                     && dateTime.getMillisOfSecond() == 0) {
                     res = dateTime.toString("yyyy-MM-dd");
                 } else if (dateTime.getMillisOfSecond() != 0) {
-                    res = dateTime.toString("yyyy-MM-dd'T'HH:mm:ss.SSS" + Util.timeZone);
+                    res = dateTime.toString("yyyy-MM-dd HH:mm:ss.SSS");
                 } else {
-                    res = dateTime.toString("yyyy-MM-dd'T'HH:mm:ss" + Util.timeZone);
+                    res = dateTime.toString("yyyy-MM-dd HH:mm:ss");
                 }
             } else if (val instanceof String) {
                 String v = ((String) val).trim();
@@ -153,9 +153,9 @@ public class ESSyncUtil {
                     if (date != null) {
                         DateTime dateTime = new DateTime(date);
                         if (dateTime.getMillisOfSecond() != 0) {
-                            res = dateTime.toString("yyyy-MM-dd'T'HH:mm:ss.SSS" + Util.timeZone);
+                            res = dateTime.toString("yyyy-MM-dd HH:mm:ss.SSS");
                         } else {
-                            res = dateTime.toString("yyyy-MM-dd'T'HH:mm:ss" + Util.timeZone);
+                            res = dateTime.toString("yyyy-MM-dd HH:mm:ss");
                         }
                     }
                 } else if (v.length() == 10 && v.charAt(4) == '-' && v.charAt(7) == '-') {
